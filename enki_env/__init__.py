7ad3d61564efd57865137392486f5541cd5256a9
Jérôme Guzzi <jerome@idsia.ch>
2025-11-25 17:47:14 +0100

    Added imports

--- enki_env/__init__.py
+++ enki_env/__init__.py
@@ -1,13 +1,35 @@
 from __future__ import annotations
 
 from .config import GroupConfig, setup_policies
-from .env import EnkiEnv
-from .parallel_env import ParallelEnkiEnv
-from .robots import (EPuckAction, EPuckObservation, MarxbotAction,
-                     MarxbotObservation, ThymioAction, ThymioObservation)
+from .env import EnkiEnv, BaseEnv
+from .parallel_env import ParallelEnkiEnv, BaseParallelEnv
+from .robots import (EPuckAction, EPuckConfig, EPuckObservation, MarxbotAction,
+                     MarxbotConfig, MarxbotObservation, ThymioAction,
+                     ThymioConfig, ThymioObservation)
+
+
+def default_config() -> dict[str, GroupConfig]:
+    """
+    Return a dictionary where default robots' names are
+    associated to robots' default configurations, i.e.:
+
+    - thymio -> :py:class:`enki_env.ThymioConfig`
+    - e-puck -> :py:class:`enki_env.EPuckConfig`
+    - marxbot -> :py:class:`enki_env.MarxbotConfig`
+
+    :returns: the dictionary of default configurations
+    """
+    rs: dict[str, GroupConfig] = {
+        'thymio': ThymioConfig(),
+        'e-puck': EPuckConfig(),
+        'marxbot': MarxbotConfig(),
+    }
+    return rs
+
 
 __all__ = [
     'GroupConfig', 'setup_policies', 'EnkiEnv', 'ParallelEnkiEnv',
     'EPuckAction', 'EPuckObservation', 'MarxbotAction', 'MarxbotObservation',
-    'ThymioAction', 'ThymioObservation'
+    'ThymioAction', 'ThymioObservation', 'EPuckConfig', 'MarxbotConfig',
+    'ThymioConfig', 'BaseEnv', 'BaseParallelEnv'
 ]
